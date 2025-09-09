4772e99ee78469f6a1ce7e0873183448a0f5bea5
Nadiar <codxse@gmail.com>
2025-09-09 08:26:21 +0700

    [C] Show file location on top right of nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,8 @@
 vim.keymap.set("n", "<leader>r", function()
   require("config.recent-files").show_recent_files()
 end, { desc = "Show Recent Files" })
+
+-- Clear file location winbar
+vim.keymap.set("n", "<leader>wc", function()
+  vim.wo.winbar = nil
+end, { desc = "Clear File Location Winbar" })
