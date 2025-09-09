d8224100537f7831d0b8cd6fd4fb0d31893ddb21
Nadiar <codxse@gmail.com>
2025-09-09 08:54:53 +0700

    [C] remove bottom location info

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,26 +3,8 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    -- 1. Define our custom file path component
-    local filepath = {
-      function()
-        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":~")
-      end,
-      cond = function()
-        return vim.api.nvim_buf_get_name(0) ~= ""
-      end,
-    }
-
-    -- 2. Redefine the center section to ONLY contain our file path
-    -- This replaces the default, which includes the navic (LSP context) component.
-    opts.sections.lualine_c = { filepath }
-
-    -- 3. Ensure the default filename component on the right is removed
-    for i, component in ipairs(opts.sections.lualine_x) do
-      if component == "filename" or (type(component) == "table" and component.name == "filename") then
-        table.remove(opts.sections.lualine_x, i)
-        break
-      end
-    end
+    -- Remove the custom file path component from the center section
+    -- Keep the default lualine configuration but remove our custom filepath
+    opts.sections.lualine_c = {}  -- Empty center section
   end,
 }
